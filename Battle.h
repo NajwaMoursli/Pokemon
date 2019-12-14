e602e84d216d305d958bf4ba951da90f8ac6bbb4
NajwaMoursli <49198711+NajwaMoursli@users.noreply.github.com>
2019-12-14 17:28:50 +0100

    Update Battle.h

--- Battle.h
+++ Battle.h
@@ -2,27 +2,27 @@
 #include "Header.h"
 #include "Game.h"
 
-// Forward declare Trainer and PlayTurn
+// Déclaration anticipée de Trainer et PlayTurn
 class Trainer;
 class PlayTurn;
 
-// Represents a battle in the game
+// Représentation d'un combat dans le jeu
 class Battle {
 
 public:
-	// Shows the battle
+	// Visualisation du combat
 	void show(sf::RenderWindow &window);
 
 private:
-	// Represents the state of the game
-		// Lose, Won, Play
+	// Status dans le Jeu
+		// Perdu, Gagné, Entrain de Jouer 
 	static BattleState stateOfGame;
 
-	// Trainers
+	// Entraineurs
 	static Trainer user;
 	static Trainer ash;
 
-	// User's pokemon
+	// Pokemon du joueur
 	static Fire uF1;
 	static Fire uF2;
 	static Water uW3;
@@ -40,38 +40,38 @@ private:
 	static Grass aG5;
 	static Grass aG6;
 
-	// The weathers
+	// Le climat
 	static DisplayedObject rain;
 	static DisplayedObject sun;
 	static DisplayedObject sand;
 
-	// Health bars
+	//Point de vie 
 	static sf::RectangleShape uH;
 	static sf::RectangleShape aH;
 
-	// Displays the health in the CMD
+	// Affiche les points de vie dans l'invite de commande (CMD)
 	static void DisplayAllHealth();
 
-	// Draws everything to screen
+	//Dessine tout à l'écran
 	static void DrawAll(sf::RenderWindow &window, sf::Sprite * background);
 
-	// Draws the weather to screen
+	//Dessine le climat à l'écran
 	static void DrawWeather(sf::RenderWindow &window);
 
-	// Initalizes images, user, and ash
+	// Initalisation des images, utilisateur et ash
 	static void InitalizeLoads();
 	static void InitalizeUser();
 	static void ResetAsh();
 
-	// Show move screen
+	// Montre les movements sur l'écran
 	static void ShowMove(sf::RenderWindow &window, sf::Sprite * background);
 	static BattleState chooseMove(sf::RenderWindow &window);
 
-	// Show switch screen
+	// Commutation de l'écran
 	static void ShowPokemon(sf::RenderWindow &window, sf::Sprite * background);
 	static BattleState choosePokemon(sf::RenderWindow &window);
 
-	// Show item screen
+	// Montre les éléments à l'écran
 	static void ShowItem(sf::RenderWindow &window, sf::Sprite * background);
 	static BattleState choosePotion(sf::RenderWindow &window);
 
