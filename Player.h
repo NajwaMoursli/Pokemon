b768e1c361b920ed241c896fd0223c87f8313459
NajwaMoursli <49198711+NajwaMoursli@users.noreply.github.com>
2019-12-16 13:12:45 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -3,20 +3,20 @@
 #include "DisplayedObject.h"
 #include <vector>
 
-// Represents a player
+// Un joeur hérite des attributs et méthodes de la class DisplayedObject pour s'en servir
 class Player : public DisplayedObject {
 
 public:
-	Player();
-	~Player();
+	Player();//Constructeur
+	~Player();//Destructeur
 
-	// Moves the player
+	// Mouvements du joueur
 	void Move(Direction direction);
 
-	// Draws the player to screen
+	// Dessine le joeur sur l'écran
 	void Draw(sf::RenderWindow &window);
 
-	friend class Game;
+	friend class Game;// permet aux membres de cette classe d'accéder à l'attributs privé de la classe Player : m_intRect
 
 private:
 	sf::IntRect m_intRect;
