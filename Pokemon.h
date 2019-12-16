04ccd9185cc1baef0834e777ac6a0f49342d219e
NajwaMoursli <49198711+NajwaMoursli@users.noreply.github.com>
2019-12-16 13:30:30 +0100

    Update Pokemon.h

--- Pokemon.h
+++ Pokemon.h
@@ -2,47 +2,46 @@
 #include "Header.h"
 #include "DisplayedObject.h"
 
-// Represents a Pokemon
+//La classe Pokemeon hérite de tous les attributs et méthode de la classe DisplayedObject
 class Pokemon : public DisplayedObject {
 
 public:
-	int m_health;
-	Type m_type;
+	int m_health;// nb point de vie 
+	Type m_type;// un type de Pokemon 
 	std::string m_name;
-	bool m_isFainted;
+	bool m_isFainted;// si Pokemon out ou encore dans la partie
 	
-	// Weather for all Pokemon
+	// Le climat dans l'ensemble du jeu 
 	static Weather weather;
 
-	Pokemon(Type type);
-	Pokemon(Type type, std::string name);
-	virtual ~Pokemon();
+	Pokemon(Type type);/// Redefinition du constructeur Pokemon pour les différents type qui en héritent
+	Pokemon(Type type, std::string name);// cas de polymorphisme 
+	virtual ~Pokemon();// Destructeur 
 
-	// Deals 20 damage
+	// Provoque 20 blessures/dégats
 	void attackNormal(Pokemon * opponentPokemon);
 
-	// Heals 20 damage
+	// Répare 20 blessures/dégats
 	void heal();
 
-	// Special attack, damage depends on the opponent
-	// type and weather
+	// Attaque spacial, les dégats dépendants de l'adversaire, du type de Pokemon et du climat
 	virtual void specialAttack(Pokemon * opponentPokemon) = 0;
 
-	// Changes the weather to that of the Pokemon's type
-	virtual void changeWeather() = 0;
+	// Changement du climat selon le type de Pokemon
+	virtual void changeWeather() = 0;// initialisation fonction virtuelle 
 
-	// Checks if the Pokemon fainted
+	// Changement Pokemon s'il est out 
 	void changeIfFainted();
 
-	// Play random attack
+	// Utiliser une attaque au hasard 
 	void playRandom(Pokemon * opponentPokemon);
 
-	// Display the health of the Pokemon
+	// Affiche les points de vie du Pokemon 
 	void displayHealth();
 
 };
 
-// Represents a Fire type Pokemon
+// Pokemon type Feu, héritage avec classe Pokemon
 class Fire : public Pokemon {
 
 public:
@@ -55,7 +54,7 @@ public:
 
 };
 
-// Represents a Water type Pokemon
+//Type Eau 
 class Water : public Pokemon {
 
 public:
@@ -68,7 +67,7 @@ public:
 
 };
 
-// Represents a Grass type Pokemon
+// Type Plante
 class Grass : public Pokemon {
 
 public:
