55bc04edd530ed3ff65050e589b93818fbee320c
NajwaMoursli <49198711+NajwaMoursli@users.noreply.github.com>
2019-12-21 10:55:18 +0100

    Update pokemonfire.cc

--- creation_des_classes_sans_sflm/pokemonfire.cc
+++ creation_des_classes_sans_sflm/pokemonfire.cc
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 #include "pokemon.hh"
-#include "pokemonFire.hh"
+#include "pokemonfire.hh"
 
 void PokemonFire::receive_dommage(const Pokemon& p){
 	if(p.get_type() == Water or p.get_type() == Ground or p.get_type() == Rock){//si le Pokemon attaque recoit une attaque
