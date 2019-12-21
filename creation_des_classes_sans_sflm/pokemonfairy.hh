6d7c189681d76ac7c89d82f187fd5c387a482a1f
NajwaMoursli <49198711+NajwaMoursli@users.noreply.github.com>
2019-12-21 11:23:51 +0100

    Create pokemonfairy.hh

---
+++ creation_des_classes_sans_sflm/pokemonfairy.hh
@@ -0,0 +1,31 @@
+#ifndef DEF_POKEMON_BUG
+
+#define DEF_POKEMON_BUG
+
+#include "pokemon.hh"
+
+
+
+class PokemonFairy : public Pokemon{
+
+public:
+
+	PokemonFairy(std::string p_name,int p_hp,int p_attack,int p_specialAttack,int p_defense,
+
+		int p_specialDefense,int p_speed):
+
+		Pokemon(p_name,Fairy,p_hp,p_attack,p_specialAttack,p_defense,p_specialDefense,p_speed){}
+
+	virtual void receive_dommage(const Pokemon& p);
+
+};
+
+
+
+
+
+
+
+
+
+#endif
