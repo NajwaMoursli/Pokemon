ae4b778a42cc73bf28f6a8367c840c396daa77d4
NajwaMoursli <49198711+NajwaMoursli@users.noreply.github.com>
2019-12-21 11:19:53 +0100

    Update type_enumerate.hh

--- creation_des_classes_sans_sflm/type_enumerate.hh
+++ creation_des_classes_sans_sflm/type_enumerate.hh
@@ -4,7 +4,7 @@
 #include <vector>
 #include <string>
 
-enum Type {Normal,Fire,Water,Grass,Electric,Ice,Dragon,Dark,Ghost,Psychic,Ground,Rock,Steel,Flying,Bug,Poison};
+enum Type {Normal,Fire,Water,Grass,Electric,Ice,Dragon,Dark,Ghost,Psychic,Ground,Rock,Steel,Flying,Bug,Poison,Fairy,Fight};
 extern std::vector<std::string> typeNames; //on declare la variable globale MAIS on l'initialise dans le .CC
 										   //sinon ca merde => (error : "multiple definitions")
 										   //extern au lieu de static car extern = globale dans tous les fichiers
